734137df4100c15fd7ef52f99ecbdd9b381e2d8c
SteveR <steve17rouse@gmail.com>
2024-02-24 21:15:48 -0700

    updated routes and paths."

--- db/routes/htmlRoutes.js
+++ db/routes/htmlRoutes.js
@@ -3,12 +3,14 @@ const router = require('express').Router();
 
 // HTML routes
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    // Correct path assuming htmlRoutes.js is inside /db/routes/
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'notes.html'));
 });
 
 // If no matching route is found default to home
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    // Correct path assuming htmlRoutes.js is inside /db/routes/
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 });
 
 // Export HTML routes
